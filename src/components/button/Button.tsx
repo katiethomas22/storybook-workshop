aa92c9c0f72d687a2aeeb61b7a27e8a1c9459bc1
katie <2756607t@student.gla.ac.uk>
2025-12-04 22:32:08 +0000

    severity

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,15 +3,20 @@ import "./Button.css";
 export interface ButtonProps {
     label: string;         //button text
     onClick?: () => void   //optional callback
+    severity?: "low" | "medium" | "high";
 }
 
 export const Button = ({
     label,
     onClick = () => alert("Button clicked with default callback!"),
+    severity = "low",
     ...rest
 }: ButtonProps) => {
+    const color = severity === "low" ? "lightgreen" : severity === "medium" ? "orange" : "red";
     return (
-        <button onClick={onClick} className="button" {...rest}>
+        <button onClick={onClick} className="button" {...rest}
+            style={{ backgroundColor: color }}
+        >
             {label}
         </button >
     );
